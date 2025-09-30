e27eecc775eeb7286b90f48c919ccda0ef90894d
Rondo Badperson <rondo_badperson@proton.me>
2025-09-30 14:30:49 +0300

    Made a matrix exercise

---
+++ matrix.py
@@ -0,0 +1,6 @@
+# using this list: 
+basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+# access "Oranges" and print it:
+# You will find the answer if you scroll down to the bottom, but attempt it yourself first!
+
+print(basket[1][1][0]) # [0] is banana, [1][0] is apples, [1][1] is [Orange], [1][2] is blueberries.
