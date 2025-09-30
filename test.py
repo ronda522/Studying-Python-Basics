cd2721af6800d4bca1ea4abc05149867d6131f61
Rondo Badperson <rondo_badperson@proton.me>
2025-09-30 14:31:39 +0300

    +_+

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-birth_year = input('what year you were born?')
+new_list = ['a', 'b', 'c']
 
-age = 2025 - int(birth_year)
-
-print(f'your age is aproximately: {age}')
+new_list[0] = 'z'
+print(new_list)
