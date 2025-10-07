d7cbae6e7418deb94b0ab8d729039ed450849ffa
Rondo Badperson <rondo_badperson@proton.me>
2025-10-07 14:48:16 +0300

    Made decorator for auth

---
+++ Decorators_auth.py
@@ -0,0 +1,23 @@
+# Create an @authenticated decorator that only allows the function to run is user1 has 'valid' set to True:
+user1 = {
+    'name': 'Sorna',
+    # changing this will either run or not run the message_friends function.
+    'valid': True
+}
+
+
+def authenticated(fn):
+    def wrapper(*args, **kwargs):
+        if user1.get('valid', True):
+            return fn(*args, **kwargs)
+        else:
+            return print("Not authorised")
+    return wrapper
+
+
+@authenticated
+def message_friends(user):
+    print(f"Message has been sent to {user['name']}")
+
+
+(message_friends(user1))
